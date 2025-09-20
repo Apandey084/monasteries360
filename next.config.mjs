0ceda2b19e8a348e24a13a0499e06482a0720795
Apandey084 <atulrkgit084@gmail.com>
2025-09-21 05:06:31 +0530

    Fix ES module error in next.config

--- next.config.mjs
+++ next.config.mjs
@@ -10,9 +10,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["encrypted-tbn0.gstatic.com", "media.istockphoto.com", "assets.zeezest.com"],
+    domains: [
+      "encrypted-tbn0.gstatic.com",
+      "media.istockphoto.com",
+      "assets.zeezest.com",
+    ],
   },
 };
 
-module.exports = nextConfig;
-
+export default nextConfig;
